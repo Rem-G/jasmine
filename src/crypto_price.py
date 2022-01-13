621c6692184dfa6f43e1686949576572e7e83ce4
pierreszelag <szelagpierre@gmail.com>
2022-01-13 15:27:00 +0100

    removed crypto_evolution + new crypto_price that get crypto data and store in db

---
+++ src/crypto_price.py
@@ -0,0 +1,37 @@
+import sys
+from datetime import datetime
+from Historic_Crypto import HistoricalData
+import numpy as np
+from service.client_mongodb import ClientDB
+
+class CryptoPrice:
+    def __init__(self, currency, start, end, addToDB = False):
+        if (addToDB):
+            self.client_db = ClientDB()
+        self.currency = currency
+        self.start = datetime.utcfromtimestamp(start).strftime("%Y-%m-%d-%H-%M")
+        self.end = datetime.utcfromtimestamp(end).strftime("%Y-%m-%d-%H-%M")
+
+        self.getDataFrame()
+        self.addDFToDB()
+
+    def getDataFrame(self):
+        self.btc_data = HistoricalData(self.currency + "-USD", 900, self.start, self.end).retrieve_data()
+        self.btc_data.reset_index(inplace=True)
+
+    def addDFToDB(self):
+        if (self.client_db is not None):
+            self.client_db.insert_documents(self.currency + "_data", self.btc_data.to_dict('records'))
+        else:
+            print(self.btc_data.tail())
+        
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 5):
+        getter = CryptoPrice(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), sys.argv[4] == "toDB")
+    elif (len(sys.argv) == 4):
+        getter = CryptoPrice(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), False)
+    else:
+        print("Usage : python crypto_price.py <crypto> <start unix timestamp> <end unix timestamp> <(optional) toDB>\n")
+        print("Ex : python crypto_price.py BTC 1546297200 1642082712 toDB")
+
